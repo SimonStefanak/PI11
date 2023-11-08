488b929a8fb8e8c056a53ecda7f612aaffb05699
Simon Stefanak <sstefanak6@gmail.com>
2023-11-08 09:11:24 +0100

    add math_random.py

---
+++ math_random.py
@@ -0,0 +1,15 @@
+#import math o = 2 * math.pi * r
+#import math as matematika o = 2 * matematika.pi * r
+#from math import o = 2 * pi * r
+from math import pi
+import random
+
+r = 10
+o = 2 * pi * r
+print(round(o, 2)) #round zaokruhli na dany pocet miest
+
+cislo = random.randrange(0, 10)
+farba = random.choice(("red", "green", "blue"))
+print (cislo)
+print(cislo2)
+print (farba)
