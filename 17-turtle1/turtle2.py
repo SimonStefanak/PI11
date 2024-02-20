63032199b494bdbf10dfb4bd2c255fbffc946f5e
kille <sstefanak6@gmail.com>
2024-02-20 20:25:29 +0100

    update turtle2.py

--- 17-turtle1/turtle2.py
+++ 17-turtle1/turtle2.py
@@ -10,18 +10,25 @@ def stvorec(dlzka):
         t.forward(dlzka)
         t.left(90)
 
+color1 = "yellow"
+color2 = "red"
 x = 0
 y = -10
 for j in range(8):
     for i in range(1,8):
+        t.fillcolor(color1)
+        t.begin_fill()
         stvorec(10)
+        t.end_fill()
         t.penup()
         t.forward(10)
         t.pendown()
         stvorec(10)
+        color1 = color2
     t.penup()
     t.setpos(x, y)
     t.pendown()
+    color2 = color1
     y = y - 10
 
 
